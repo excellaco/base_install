74c69801dea09d5b3d6c080bba290cc835d34777
Bryan Cash <bryan_cash@Bryans-MacBook-Pro.local>
2020-02-13 15:30:34 -0500

    BAC: Update Dockerfile & docker entrypoint sh script

--- data_science_abstract/docker-entrypoint.sh
+++ data_science_abstract/docker-entrypoint.sh
@@ -2,9 +2,4 @@
 
 set -e
 
-if [ $# -eq 0 ]
-     then
-          jupyter notebook --no-browser --allow-root --port 8888 --ip 0.0.0.0 --NotebookApp.token='local-development'
-     else
-          exec "$@"
-fi
+jupyter notebook --no-browser --allow-root --port 8888 --ip 0.0.0.0 --NotebookApp.token='local-development'
