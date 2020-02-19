380f9593b096092b1fba56e2a06f0763e9bbdfab
Bryan Cash <bryan_cash@Bryans-MacBook-Pro.local>
2020-02-19 16:08:50 -0500

    BAC: Make repository directory dynamic as well

--- data_science_abstract/build.sh
+++ data_science_abstract/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build . --no-cache --build-arg SSH_PRIVATE_KEY="`cat housing_prices_id_rsa`" --build-arg REPO_TO_CLONE="git@github.com:bcashofficial/housing_prices.git" -t base_install_setup
+docker build . --no-cache --build-arg SSH_PRIVATE_KEY="`cat housing_prices_id_rsa`" --build-arg REPO_TO_CLONE="git@github.com:bcashofficial/housing_prices.git" --build-arg REPOSITORY_DIR="housing_prices" -t base_install_setup
