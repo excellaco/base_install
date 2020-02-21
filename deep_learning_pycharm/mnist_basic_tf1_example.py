e4f11784e406ee3dadbd359afe36b75bae7bc8a7
Bryan Cash <bryan_cash@Bryans-MacBook-Pro.local>
2020-02-21 10:15:25 -0500

    BAC: Made some slight modifications while running locally. Will integrate this with docker image in data_science_abstract folder

--- deep_learning_pycharm/mnist_basic_tf1_example.py
+++ deep_learning_pycharm/mnist_basic_tf1_example.py
@@ -1,8 +1,13 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
 import tensorflow as tf
 from datetime import datetime
+
 gpus = tf.config.experimental.list_physical_devices('GPU')
 print(gpus)
+
 if gpus:
     try:
         # Currently, memory growth needs to be the same across GPUs
@@ -14,22 +19,14 @@ if gpus:
         # Memory growth must be set before GPUs have been initialized
         print(e)
 
+
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(512, activation=tf.nn.relu),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-])
-
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)), tf.keras.layers.Dense(512, activation=tf.nn.relu), tf.keras.layers.Dropout(0.2), tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 log_dir="logs/profile/" + datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1, profile_batch = 3)
